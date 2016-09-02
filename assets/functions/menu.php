b1de03f4145457e9920352b21a7c6344620387a0
encelado748 <vinnocenti@outlook.it>
2016-09-02 13:01:09 +0200

    Nested submenu in off-canvas

--- assets/functions/menu.php
+++ assets/functions/menu.php
@@ -18,7 +18,7 @@ function joints_top_nav() {
         'fallback_cb' => false,                         // Fallback function (see below)
         'walker' => new Topbar_Menu_Walker()
     ));
-} 
+}
 
 // Big thanks to Brett Mason (https://github.com/brettsmason) for the awesome walker
 class Topbar_Menu_Walker extends Walker_Nav_Menu {
@@ -39,12 +39,12 @@ function joints_off_canvas_nav() {
         'fallback_cb' => false,                         // Fallback function (see below)
         'walker' => new Off_Canvas_Menu_Walker()
     ));
-} 
+}
 
 class Off_Canvas_Menu_Walker extends Walker_Nav_Menu {
     function start_lvl(&$output, $depth = 0, $args = Array() ) {
         $indent = str_repeat("\t", $depth);
-        $output .= "\n$indent<ul class=\"vertical menu\">\n";
+        $output .= "\n$indent<ul class=\"vertical menu nested\">\n";
     }
 }
 
