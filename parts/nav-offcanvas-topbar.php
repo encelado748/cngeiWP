d004d3db7fc404532185842d93a9fee8c4bc8041
encelado748 <vinnocenti@outlook.it>
2016-09-03 16:36:19 +0200

    Responsive Menu icon

--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -7,12 +7,12 @@
 			<li><img src="<?php echo get_bloginfo('template_directory');?>/assets/images/logo.svg"><h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1></li>
 		</ul>
 	</div>
-	<div class="top-bar-right show-for-medium">
+	<div class="top-bar-right show-for-large">
 		<?php joints_top_nav(); ?>
 	</div>
-	<div class="top-bar-right float-right show-for-small-only">
+	<div class="top-bar-right float-right hide-for-large">
 		<ul class="menu">
-			<li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
+			<li><button class="responsive-menu-icon" type="button" data-toggle="off-canvas"></button></li>
 			<!-- <li><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li> -->
 		</ul>
 	</div>
